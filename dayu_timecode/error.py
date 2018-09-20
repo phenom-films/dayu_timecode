744f87f5c0fcbf123b95ac3e19ce9a9d8d0f72d4
gxagxagxa <gxagxagxa@me.com>
2018-09-21 00:52:21 +0800

    add some DayuTimeRange...Error class

--- dayu_timecode/error.py
+++ dayu_timecode/error.py
@@ -8,3 +8,19 @@ import exceptions
 
 class DayuTimeCodeValueError(exceptions.ValueError):
     pass
+
+
+class DayuTimeRangeValueError(exceptions.ValueError):
+    pass
+
+
+class DayuTimeRangeFpsNotIdenticalError(exceptions.ValueError):
+    pass
+
+
+class DayuTimeRangeOperationError(exceptions.ValueError):
+    pass
+
+
+class DayuTimeRangeOutOfRange(exceptions.ValueError):
+    pass
